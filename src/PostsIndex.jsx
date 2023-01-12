5ce849add7afdce36b95f346570644fce3d425cf
Makayla <makaylabusher@gmail.com>
2023-01-12 13:09:40 -0600

    add card style to index

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -7,12 +7,23 @@ export function PostsIndex(props) {
 
       {props['posts'].map(post => (
         <div className="posts" key={post.id}>
-          <h2>{post.title}</h2>
-          <p><img src={post.image}></img></p>
-          <button type="button" className="btn btn-dark" onClick={() => props.onSelectPost(post)}>Show Post</button>
+          <div className="card mb-3" >
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={post.image} className="img-fluid rounded-start" alt="..."></img>
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">{post.title}</h5>
+                  <button type="button" className="btn btn-dark" onClick={() => props.onSelectPost(post)}>Show Post</button>
+                  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
-
     </div>
   );
 }
+
