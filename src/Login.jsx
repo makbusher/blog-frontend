284176a40b7f756f1cc113f55a358eb2e5e50c8b
Makayla <makaylabusher@gmail.com>
2023-01-13 13:07:52 -0600

    login JS form + errors/setErrors for Signup

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,37 @@
+import axios from "axios";
+import {useState} from 'react';
+
+const jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+}
+
 export function Login() {
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors([]);
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/sessions.json", params)
+      .then((response) => {
+        console.log(response.data);
+        axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+        localStorage.setItem("jwt", response.data.jwt);
+        event.target.reset();
+        
+      })
+      .catch((error) => {
+        console.log(error.response);
+        setErrors(["Invalid email or password"]);
+      });
+  };
+
   return (
     <div>
       <h2>Login</h2>
-      <form action="http://localhost:3000/sessions.json" method="POST">
+      <form onSubmit={handleSubmit}>
         <p>Email: <input name="email" type="email"/></p>
         <p>Password: <input name="password" type="password"/></p>
         <button type="submit">Login</button>
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,15 +1,19 @@
 import axios from "axios";
+import { useState } from 'react';
 
 export function Signup() {
+  const [errors, setErrors] = useState([]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event.target);
+    setErrors([]);
     const params = new FormData(event.target);
     axios.post("http://localhost:3000/users.json", params).then(response => {
       console.log(response.data);
       event.target.reset();
     })
       .catch(error => {
+        console.log("in the catch!!!");
         console.log(error.response.data.errors);
       });
   };
@@ -19,9 +23,9 @@ export function Signup() {
       <h2>Signup</h2>
       <form onSubmit={handleSubmit}>
         <p>Name: <input type="text" name="name"/></p>
-        <p>Email: <input type="text" name="email"/></p>
-        <p>Password: <input type="text" name="password"/></p>
-        <p>Password Confirmation: <input type="text" name="password_confirmation"/></p>
+        <p>Email: <input type="email" name="email"/></p>
+        <p>Password: <input type="password" name="password"/></p>
+        <p>Password Confirmation: <input type="password" name="password_confirmation"/></p>
         <button type="submit">Signup</button>
       </form>
     </div>
